ae766227c3ed4ff6a5a9a1dd879e54afbe2328e1
无敌joker <17775369906@163.com>
2025-08-29 10:47:21 +0800

    禁用员工功能的开发

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -104,4 +104,13 @@ public class EmployeeServiceImpl implements EmployeeService {
         return new PageResult(total,result);
     }
 
+    @Override
+    public void startOrStop(Integer status, Long id) {
+        Employee employee = Employee.builder()
+            .status(status)
+            .id(id)
+            .build();
+
+        employeeMapper.update(employee);
+    }
 }
