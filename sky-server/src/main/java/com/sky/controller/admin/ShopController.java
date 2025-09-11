aaea6ceafa788370b6eb5ddc2519525592d39f85
无敌joker <17775369906@163.com>
2025-09-11 16:57:13 +0800

    feat:使用Redis实现设置店铺状态的功能，并将接口文档分为管理端和用户端

---
+++ sky-server/src/main/java/com/sky/controller/admin/ShopController.java
@@ -0,0 +1,44 @@
+package com.sky.controller.admin;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sky.result.Result;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@Api(tags = "店铺相关接口")
+@RequestMapping("/admin/shop")
+public class ShopController {
+
+
+    private static final String KEY = "SHOP_STATUS";
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @PutMapping("/{status}")
+    @ApiOperation("店铺状态")
+    public Result setStatus(@PathVariable Integer status) {
+        log.info("店铺状态:{}",status == 1 ? "营业中":"打烊中");
+        redisTemplate.opsForValue().set(KEY,status);
+        return Result.success();
+    }
+
+    @GetMapping("/status")
+    @ApiOperation("管理员获取店铺的营业状态")
+    public Result<Integer> getStatus() {
+        Integer status = (Integer) redisTemplate.opsForValue().get(KEY);;
+        log.info("获取到的店铺状态 :{}",status == 1 ? "营业中" :"打烊中");
+        return Result.success(status);
+    }
+}
